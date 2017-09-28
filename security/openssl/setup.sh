d620eaa73e21b318d0ce27ae583953961c5b4404
Dragos Oancea <oancea.dragosh@gmail.com>
2017-09-28 09:45:02 +0200

    update scripts

--- security/openssl/setup.sh
+++ security/openssl/setup.sh
@@ -3,9 +3,11 @@
 DOMAIN=${1:-domain}
 PASSWORD=veryS3cr3tpassw0rd
 VALIDITY=365
+
 echo "Domain name: $DOMAIN";
 echo "OpenSSL: `openssl version -a`"
 echo "-----------------------------"
+mkdir -p out
 
 KEY="out/${DOMAIN}.key"
 CRT="out/${DOMAIN}.crt"
